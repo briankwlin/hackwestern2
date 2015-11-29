6be0890b0be4af2110c72e436010acbd405f19a7
Emily Goose <EmilyGoose@users.noreply.github.com>
2015-11-29 14:30:44 -0500

    DELETE

--- cfg.py
+++
@@ -1,5 +0,0 @@
-HOST = "irc.twitch.tv"              # the Twitch IRC server
-PORT = 6667                         # always use port 6667!
-NICK = "twitchplayssketchbot"       # Twitch username
-PASS = "REDACTED"                   # your Twitch OAuth token (Removed for obvious reasons)
-CHAN = "#twitchplayssketchbot"      # the channel you want to join
